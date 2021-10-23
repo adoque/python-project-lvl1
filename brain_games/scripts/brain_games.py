4bc77ad0deacc46a8102ade63198590bc82d63eb
stereoyana <rabotabot@gmail.com>
2021-10-23 21:04:53 +0300

    changes after linter blame

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -3,10 +3,12 @@
 from brain_games.cli import welcome_user
 from brain_games.brain_even import check_even
 
+
 def main():
     print('Welcome to the Brain Games!')
     name = welcome_user()
     check_even(name)
+
 
 if __name__ == '__main__':
     main()
