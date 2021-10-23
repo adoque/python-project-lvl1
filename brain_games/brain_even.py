a1bbb8c500ff638e5a16d232a9a46e515fd98013
stereoyana <rabotabot@gmail.com>
2021-10-23 20:50:39 +0300

    adding even game

---
+++ brain_games/brain_even.py
@@ -0,0 +1,28 @@
+import prompt
+import random
+
+
+def check_even(name):
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+
+    for i in range(3):
+        number = random.randint(1, 100)
+        even = number % 2 == 0
+        odd = number % 2 != 0
+
+        print('Question: {}'.format(number))
+        answer = prompt.string('Your answer: ')
+
+        if (even and answer == 'yes') or (odd and answer == 'no'):
+            print('Correct!')
+            if i == 2:
+                print('Congratulations, {}!'.format(name))
+                break
+        elif even and answer != 'yes':
+            print("'{}' is wrong answer ;(. Correct answer was 'yes'.".format(answer))
+            print("Let's try again, {}".format(name))
+            break
+        elif odd and answer != 'no':
+            print("'{}' is wrong answer ;(. Correct answer was 'no'.".format(answer))
+            print("Let's try again, {}".format(name))
+            break
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env
 
 from brain_games.cli import welcome_user
-
+from brain_games.brain_even import check_even
 
 def main():
     print('Welcome to the Brain Games!')
-    welcome_user()
-
+    name = welcome_user()
+    check_even(name)
 
 if __name__ == '__main__':
     main()
