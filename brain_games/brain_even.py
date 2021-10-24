519d9b148c5afd575d79a30c9abd4d332439c4f2
stereoyana <rabotabot@gmail.com>
2021-10-24 18:33:58 +0300

    minor changes

--- brain_games/brain_even.py
+++ brain_games/brain_even.py
@@ -19,12 +19,12 @@ def check_even(name):
                 print('Congratulations, {}!'.format(name))
                 break
         elif even and answer != 'yes':
-            print("'{}' is wrong answer ;(.' \
-            'Correct answer was 'yes'.".format(answer))
+            print("'{}' is wrong answer ;(. "
+            "Correct answer was 'yes'.".format(answer))
             print("Let's try again, {}!".format(name))
             break
         elif odd and answer != 'no':
-            print("'{}' is wrong answer ;(.' \
-            'Correct answer was 'no'.".format(answer))
+            print("'{}' is wrong answer ;(. "
+            "Correct answer was 'no'.".format(answer))
             print("Let's try again, {}!".format(name))
             break
